2044ad3a0bdb631300f76fdf4438586937257d92
Theo Santos <theo.dellacqua@gmail.com>
2024-09-22 22:00:43 -0300

    Exercicio 1.2 implementado

--- estrutura_sequencial_exercicios.sql
+++ estrutura_sequencial_exercicios.sql
@@ -14,6 +14,20 @@ BEGIN
 END;
 $$
 
+--Exercício 1.2
+DO
+$$
+DECLARE
+    n1 NUMERIC(10,2);
+    limite_inferior INTEGER := 1;
+    limite_superior INTEGER := 10;
+BEGIN
+    n1 := limite_inferior + (random() * (limite_superior - limite_inferior + 0.01))::numeric;
+    RAISE NOTICE '%', n1;
+END
+$$
+
+
 
 
 
